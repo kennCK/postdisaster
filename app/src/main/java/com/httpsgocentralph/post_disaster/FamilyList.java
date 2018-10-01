1a7a224223f83ce86ad19d785b62ffdf475423e0
Kennette <kennettecanales@gmail.com>
2018-10-01 12:01:49 +0800

    update

---
+++ app/src/main/java/com/httpsgocentralph/post_disaster/FamilyList.java
@@ -0,0 +1,13 @@
+package com.httpsgocentralph.post_disaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FamilyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family_list);
+    }
+}
