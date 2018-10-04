9d57e88fe29bc5869eed07d05381d926895bf738
Kennette <kennettecanales@gmail.com>
2018-10-04 17:42:12 +0800

    update

--- app/src/main/java/com/httpsgocentralph/post_disaster/Fragment/SendFragment.java
+++ app/src/main/java/com/httpsgocentralph/post_disaster/Fragment/SendFragment.java
@@ -1,19 +1,103 @@
 package com.httpsgocentralph.post_disaster.Fragment;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.httpsgocentralph.post_disaster.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static android.content.ContentValues.TAG;
+
 public class SendFragment extends Fragment {
+    BluetoothAdapter bluetoothAdapter;
+    ListView listView;
+    IntentFilter filter;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
+    BroadcastReceiver mReceiver;
+    View view;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_send, container, false);
+        view =  inflater.inflate(R.layout.fragment_send, container, false);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        listView = (ListView)view.findViewById(R.id.listViewSend);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        if(bluetoothAdapter.isDiscovering()){
+            bluetoothAdapter.cancelDiscovery();
+        }
+        checkBTPermissions();
+        bluetoothAdapter.startDiscovery();
+        Log.d(TAG, "STARTING DEVICES");
+        setup();
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(mReceiver);
+    }
+
+    private void setup(){
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent
+                            .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    mDeviceList.add(device.getName() + "\n" + device.getAddress());
+                    Log.d(TAG, "DEVICES FOUND");
+                    listView.setAdapter(new ArrayAdapter<String>(context,
+                            android.R.layout.simple_list_item_1, mDeviceList));
+                }else{
+                    Log.d(TAG, "NOT DEVICE FOUND");
+                }
+            }
+        };
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkBTPermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = getActivity().checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += getActivity().checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
     }
 }
